f2edfb90b892cd6332cfa3ccb67d3292e4cc83d1
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-19 21:56:01 +0300

    [Chore] Add reducer to initiate books object

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Javascript: The good parts'},
+        {title: 'Harry Porter'},
+        {title: 'The dark Tower'},
+        {title: 'Eloquent Ruby'},
+        {title: 'Another One'}
+    ]
+}
