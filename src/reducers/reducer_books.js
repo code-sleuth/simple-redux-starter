48497242910c6c7e6cd77364a92f8b5493df6b42
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-20 12:17:31 +0300

    [Chore] Add pages attribute to object

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        {title: 'Javascript: The good parts'},
-        {title: 'Harry Porter'},
-        {title: 'The dark Tower'},
-        {title: 'Eloquent Ruby'},
-        {title: 'Another One'}
+        {title: 'Javascript: The good parts', pages: 101},
+        {title: 'Harry Porter', pages: 39},
+        {title: 'The dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1},
+        {title: 'Another One', pages: 1}
     ]
 }
